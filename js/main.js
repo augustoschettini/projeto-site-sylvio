e1bfe9b8d17177b655c9c9ac034df5106a634603
Augusto Schettini <Augusto Schettini>
2016-11-06 03:28:45 -0200

    Ajustes diversos, add JS nas páginas

--- js/main.js
+++ js/main.js
@@ -7,3 +7,19 @@ $("#slider").owlCarousel({
       responsive : true,
  
   });
+//$("ul.navbar-nav > li > a").on('click', function(event){
+    //event.preventDefault();
+  //  $("html, body").animate({scrollTop:$(this.hash).offset().top-50}, 1200);
+//});
+
+
+$(window).scroll(function(){
+    var TopMenu = $(this).scrollTop();
+    
+    if (TopMenu > 200) {
+        $('#navegacao').addClass('navegacao-fixa');
+    }
+    else{
+        $('#navegacao').removeClass('navegacao-fixa');
+    }
+})
